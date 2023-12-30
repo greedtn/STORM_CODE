4c25c8a20371e012b3f1e9afe200319698910665
naoto <naoto.tsuda63@gmail.com>
2023-12-31 04:18:53 +0900

    著者の係数で1,000年分算出

---
+++ Execution/function/MASTER_ORIGINAL.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Nadia Bloemendaal, nadia.bloemendaal@vu.nl
+
+For more information, please see 
+Bloemendaal, N., Haigh, I.D., de Moel, H. et al. 
+Generation of a global synthetic tropical cyclone hazard dataset using STORM. 
+Sci Data 7, 40 (2020). https://doi.org/10.1038/s41597-020-0381-2
+
+This is the STORM model master program
+
+Copyright (C) 2020 Nadia Bloemendaal. All versions released under the GNU General Public License v3.0.
+"""
+
+import numpy as np
+
+#Custom made modules
+from SELECT_BASIN import Basins_WMO
+
+from SAMPLE_STARTING_POINT import Startingpoint
+from SAMPLE_TC_MOVEMENT import TC_movement
+from SAMPLE_TC_PRESSURE import TC_pressure_original
+
+import os
+import sys
+dir_path=os.path.dirname(os.path.realpath(sys.argv[0]))
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+# 現在のファイルのディレクトリパスを取得
+current_dir = os.path.dirname(__file__)
+# output_data ディレクトリへの相対パスを作成
+output_dir = os.path.join(current_dir, '..', 'output_data')
+
+import time
+start_time=time.time()
+
+#==============================================================================
+# Step 1: Define basin and number of years to run
+#==============================================================================
+#please set basin (EP,NA,NI,SI,SP,WP)
+basin='WP'
+loop=1 #ranges between 0 and 9 to simulate slices of 1000 years
+
+total_years=50 #set the total number of years you'd like to simulate
+
+TC_data=[] #This list is composed of: [year,storm number,lat,lon,pressure,wind,rmax,category,Holland B parameter,precipitation,landfall flag]
+#==============================================================================
+#     Step 2: load grid with weighted genesis counts
+#==============================================================================
+for year in range(0,total_years):
+    print(year + 1, "年目スタート")
+    storms_per_year,genesis_month,lat0,lat1,lon0,lon1=Basins_WMO(basin) 
+
+    if storms_per_year>0:
+            #==============================================================================
+            # Step 3: Generate (list of) genesis locations
+            #==============================================================================
+            lon_genesis_list,lat_genesis_list=Startingpoint(storms_per_year,genesis_month,basin) 
+                            
+            #==============================================================================
+            # Step 4: Generate initial conditions    
+            #==============================================================================
+            latlist,lonlist,landfalllist=TC_movement(lon_genesis_list,lat_genesis_list,basin)
+                    
+            TC_data=TC_pressure_original(basin,latlist,lonlist,landfalllist,year,storms_per_year,genesis_month,TC_data)
+
+TC_data=np.array(TC_data)
+np.savetxt(os.path.join(output_dir,'STORM_DATA_IBTRACS_'+str(basin)+'_'+str(total_years)+'_YEARS_'+str(loop)+'_Original.txt'),TC_data,fmt='%5s',delimiter=',')
